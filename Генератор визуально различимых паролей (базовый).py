4fc1bfccd9137a3ab3490d81b037b048734b9053
alex <106116143+maslov-cool@users.noreply.github.com>
2024-03-06 00:29:35 +0300

    Update Генератор визуально различимых паролей (базовый).py
    Требуется по запросу выдавать 
    N
     различных паролей длиной 
    M
     символов, состоящих из строчных и прописных латинских букв и цифр, кроме тех, которые легко перепутать между собой: «l» (L маленькое), «I» (i большое), «1» (цифра), «o» и «O» (большая и маленькая буквы) и «0» (цифра).
    
    Дополнительное условие: в каждом пароле обязательно должна присутствовать хотя бы одна цифра и как минимум по одной букве в верхнем и нижнем регистре, причём все символы должны быть различны.
    
    Решение должно содержать две функции: вспомогательную generate_password(m), возвращающую случайный пароль длиной 
    m
     символов, и основную main(n, m), возвращающую список из 
    n
     различных паролей, каждый длиной 
    m
     символов.

--- Генератор визуально различимых паролей (базовый).py
+++ Генератор визуально различимых паролей (базовый).py
@@ -1,19 +1,26 @@
-import string
 import random
+import string
+
+
+big_letters = [i for i in string.ascii_uppercase if i not in 'OI']
+small_letters = [i for i in string.ascii_lowercase if i not in 'ol']
+digits = [i for i in string.digits if i not in '01']
 
 
 def generate_password(m):
-    return ''.join([random.choice(''.join(i for i in string.ascii_letters.upper() if i not in 'IO') +
-                                  ''.join(i for i in string.ascii_letters.lower() if i not in 'lo')
-                                  + ''.join(i for i in string.digits if i not in '10'))
-                    for _ in range(m)])
+    global big_letters, small_letters, digits
+    password = set(random.choices(big_letters + small_letters + digits, k=m - 3))
+    password.add(random.choice(big_letters))
+    password.add(random.choice(small_letters))
+    password.add(random.choice(digits))
+    while len(password) != m:
+        password.add(random.choice(big_letters + small_letters + digits))
+    return ''.join(password)
 
 
 def main(n, m):
-    set_ = set()
+    set_ = {generate_password(m) for _ in range(n)}
     while len(set_) != n:
-        set_.add(generate_password(m))
-    return set_
-print("Случайный пароль из 7 символов:", generate_password(7))
-print("10 случайных паролей длиной 15 символов:")
-print(*main(10, 15), sep="\n")
+        set_.add(random.choice([generate_password(m), ''.join(random.sample(random.choice(list(set_)), m))]))
+    return list(set_)
+
